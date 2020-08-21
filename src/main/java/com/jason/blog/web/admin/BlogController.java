8f0b01ae10b4bc38c675c111175be0f88b47f6ec
jasonchen96 <397390540@qq.com>
2020-08-20 17:54:05 -0700

    draft version

--- src/main/java/com/jason/blog/web/admin/BlogController.java
+++ src/main/java/com/jason/blog/web/admin/BlogController.java
@@ -1,26 +1,39 @@
 package com.jason.blog.web.admin;
 
+import com.jason.blog.po.Blog;
+import com.jason.blog.po.User;
+import com.jason.blog.service.BlogService;
+import com.jason.blog.service.TagService;
+import com.jason.blog.service.TypeService;
+import com.jason.blog.vo.BlogQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class BlogController {
     private static final String LIST = "admin/blogs";
     private static final String REDIRECT_LIST = "redirect:/admin/blogs";
+    private static final String INPUT = "admin/blogs-input";
 
 
     @Autowired
-//    private BlogService blogService;
-//    @Autowired
-//    private TypeService typeService;
-//    @Autowired
-//    private TagService tagService;
+    private BlogService blogService;
+    @Autowired
+    private TypeService typeService;
+    @Autowired
+    private TagService tagService;
 
     @GetMapping("/blogs")
     public String blogs(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
